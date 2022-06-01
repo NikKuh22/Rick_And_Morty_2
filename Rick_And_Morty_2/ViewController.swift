d73144260dbd819a14d5a892f2278250c220ed65
NikKuh22 <nikuh1018@gmail.com>
2022-06-01 13:19:33 +0300

    Initial Commit

---
+++ Rick_And_Morty_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick_And_Morty_2
+//
+//  Created by Никита Анонимов on 01.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
