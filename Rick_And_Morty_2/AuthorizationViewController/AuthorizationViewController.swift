96a0cbbad79f203624a44c89e405586c73f1181a
NikKuh22 <nikuh1018@gmail.com>
2022-07-21 16:04:57 +0300

    final

--- Rick_And_Morty_2/AuthorizationViewController/AuthorizationViewController.swift
+++ Rick_And_Morty_2/AuthorizationViewController/AuthorizationViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 import Firebase
 
 final class AuthorizationController {
-    func logicForAuth(user: String, password: String, completion: @escaping (_ success: Bool) -> Void) {
-        Auth.auth().signIn(withEmail: user, password: password) { result, error in
+    var email = ""
+    var password = ""
+    
+    func logicForAuth(completion: @escaping (_ success: Bool) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
             if error != nil {
                 completion(false)
 //                print("Error \(error)")
@@ -24,7 +27,6 @@ final class AuthorizationController {
     }
 }
 
-
 final class AuthorizationViewController: UIViewController {
     
     var controller = AuthorizationController()
@@ -32,19 +34,23 @@ final class AuthorizationViewController: UIViewController {
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var buttonEnter: UIButton!
+    @IBOutlet var bottomConstraint: NSLayoutConstraint!
     
-    @IBAction func loginEnterButton(_ sender: Any) {
+    @IBAction func loginEnterButton() {
         buttonEnter.configuration?.showsActivityIndicator = true
-        controller.logicForAuth(user: emailTextField.text!, password: passwordTextField!.text!) { success in
+        buttonEnter.isUserInteractionEnabled = false
+        controller.logicForAuth { [weak self] success in
+            self?.buttonEnter.configuration?.showsActivityIndicator = false
+            self?.buttonEnter.isUserInteractionEnabled = true
+            
             if success == true {
-                let charactersViewController = UIStoryboard (name: "Main", bundle: .main).instantiateViewController(withIdentifier: "MainSplitViewController") as! MainSplitViewController
-                self.present(charactersViewController, animated: true)
+                let charactersViewController = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "MainSplitViewController") as! MainSplitViewController
+                self?.present(charactersViewController, animated: true)
             } else {
                 let alert = UIAlertController(title: "Oops...", message: "Incorrectly entered password or email", preferredStyle: .alert)
                 print("Error")
                 alert.addAction(UIAlertAction(title: "Ok", style: .default))
-                self.present(alert, animated: true)
-                self.buttonEnter.configuration?.showsActivityIndicator = false
+                self?.present(alert, animated: true)
             }
         }
     }
@@ -55,51 +61,88 @@ final class AuthorizationViewController: UIViewController {
         passwordTextField.delegate = self
         buttonEnter.isEnabled = false
         
+        buttonEnter.configuration?.imagePadding = 16
+        
         let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tapGesture)
         
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillShow(notificaton:)),
-                                               name: UIResponder.keyboardWillShowNotification,
-                                               object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
 
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillHide),
-                                               name: UIResponder.keyboardWillHideNotification,
-                                               object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
     }
 }
 
 extension AuthorizationViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        passwordTextField.becomeFirstResponder()
+        if textField === emailTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField === passwordTextField {
+            textField.resignFirstResponder()
+        }
+        
         return true
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let emailText = emailTextField.text
-        let passwordText = passwordTextField.text
+        guard var text = textField.text else { return true }
         
-        if emailText?.isEmpty == true || passwordText?.isEmpty == true {
-            buttonEnter.isEnabled = false
-        } else {
-            buttonEnter.isEnabled = true
+        let firstIndex = text.index(text.startIndex, offsetBy: range.lowerBound)
+        let lastIndex = text.index(text.startIndex, offsetBy: range.upperBound)
+        text.replaceSubrange(firstIndex..<lastIndex, with: string)
+        
+//        print(text)
+        
+        if textField === emailTextField {
+            controller.email = text
+        } else if textField === passwordTextField {
+            controller.password = text
         }
+        
+        buttonEnter.isEnabled = !controller.email.isEmpty && !controller.password.isEmpty
         return true
     }
 }
 
 extension AuthorizationViewController {
-    
-    @objc private func keyboardWillShow(notificaton: NSNotification) {
-        if let keyboardFrame: NSValue = notificaton.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-            let keyboardHeight = keyboardFrame.cgRectValue.height
-            let textField = self.view.frame.height - (passwordTextField.frame.origin.y - passwordTextField.frame.height)
-            self.view.frame.origin.x -= keyboardHeight - textField
+    @objc private func keyboardWillShow(notification: Notification) {
+        guard let keyboardEndFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+              let keyboardAnimationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval,
+              let animationCurveRaw = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Int,
+              let animationCurve = UIView.AnimationCurve(rawValue: animationCurveRaw) else {
+            return
         }
+        bottomConstraint.constant = keyboardEndFrame.height + 16
+        
+        let animator = UIViewPropertyAnimator(duration: keyboardAnimationDuration, curve: animationCurve) {
+            self.view.layoutIfNeeded()
+        }
+        
+        animator.startAnimation()
     }
     
-    @objc private func keyboardWillHide() {
-        self.view.frame.origin.y = 0
+    @objc private func keyboardWillHide(notification: Notification) {
+        guard let keyboardAnimationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval,
+              let animationCurveRaw = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Int,
+              let animationCurve = UIView.AnimationCurve(rawValue: animationCurveRaw) else {
+            return
+        }
+        
+        bottomConstraint.constant = 16
+        
+        let animator = UIViewPropertyAnimator(duration: keyboardAnimationDuration, curve: animationCurve) {
+            self.view.layoutIfNeeded()
+        }
+        
+        animator.startAnimation()
     }
 }
